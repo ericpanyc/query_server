9b462e1f288d987eda852c85464f922f3a3da3ab
ericpanyc <yachenpan1005@gmail.com>
2019-09-15 20:02:00 -0700

    new version

--- query_server.sh
+++ query_server.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
-if [ -z "$1" ]; then
-    echo Current Server: $HOSTNAME
-elif [ $1 = '-l' ]; then
-    echo Current Server: $USER'@'$HOSTNAME
-elif [ $1 = '--help' ]; then
-    echo 'Usage: query_server.sh [OPTION]' 
-    echo ""
-    echo 'Prints the name of the current server as: "Current Server:[servername]'
-    echo ""
-    echo '   -l  Prints the username with the servername'
-    echo ""
-    echo 'Exit status:'
-    echo ' 0  if OK,'
-    echo ' 1  Problem with command line'
-else 
-    echo 'exit'
+if [ -z "$1" ]; then echo Current Server: $HOSTNAME
+else
+
+    OPTS=`getopt -o l --long help -n 'parse-options' -- "$@"`
+    if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+    eval set -- "$OPTS"
+
+    help_message="Usage: query_server.sh [OPTION]\n\nPrints the name of the current server as: Current Server:[servername]\n\n   -l  Prints the username with the servername\n\nExit status:\n 0  if OK,\n 1  Problem with command line"
+
+    while true; do
+      case "$1" in
+        -l ) echo Current Server: $USER@$HOSTNAME; shift ;;
+        --help ) echo -e $help_message; shift ;;
+        * ) exit 1 ;;
+      esac
+    done
 fi
